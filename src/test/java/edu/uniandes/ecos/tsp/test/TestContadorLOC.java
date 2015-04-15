63b0639cee61767d8225a3923c325b0ea3bac410
Daniel Renteria <df.renteria10@uniandes.edu.co>
2015-04-15 13:54:01 -0500

    Pruebas Unitarias Contador LOC

--- src/test/java/edu/uniandes/ecos/tsp/test/TestContadorLOC.java
+++ src/test/java/edu/uniandes/ecos/tsp/test/TestContadorLOC.java
@@ -1,17 +1,48 @@
 package edu.uniandes.ecos.tsp.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
 
 import org.junit.Test;
 
+import edu.uniandes.ecos.tsp.modelo.ContadorLOC;
+
 public class TestContadorLOC {
 
+	private ContadorLOC contador;
+	
+	private String getRutaArchivoTexto(){
+		ClassLoader classLoader = this.getClass().getClassLoader();
+    	return classLoader.getResource("LOCCountTest.txt").getFile();
+	}
+	
+	private String getRutaCodigoFuente(){
+		ClassLoader classLoader = this.getClass().getClassLoader();
+    	return classLoader.getResource("Test.java").getFile();
+	}
+	
 	@Test
 	public void testContarLineasDeCodigo() {
-	}
-
-	@Test
-	public void testAgruparLineasMetodo() {
+		try {
+			contador = new ContadorLOC();
+			//Archivo de Texto
+			contador.contarLineasDeCodigo(getRutaArchivoTexto());
+			assertEquals(12, contador.getLineasTotales());
+			assertEquals(6, contador.getLineasEfectivas());
+			
+			contador = new ContadorLOC();
+			//Archivo Java
+			contador.contarLineasDeCodigo(getRutaCodigoFuente());
+			assertEquals(21, contador.getLineasTotales());
+			assertEquals(11, contador.getLineasEfectivas());
+			assertEquals(3, contador.getNombresDeMetodos().size());
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
 	}
 
 	@Test
@@ -19,27 +50,15 @@ public class TestContadorLOC {
 	}
 
 	@Test
-	public void testGetLineasTotales() {
-	}
-
-	@Test
-	public void testGetLineasEfectivas() {
-	}
-
-	@Test
 	public void testGetNombreArchivoFuente() {
-	}
-
-	@Test
-	public void testGetConteoMetodos() {
-	}
-
-	@Test
-	public void testGetNombresDeMetodos() {
-	}
-
-	@Test
-	public void testGetLineasDeMetodos() {
+		try {
+			contador = new ContadorLOC();
+			contador.contarLineasDeCodigo(getRutaCodigoFuente());
+			assertEquals("Test.java", contador.getNombreArchivoFuente());
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
 	}
 
 }
